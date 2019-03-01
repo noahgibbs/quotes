4299d46d24ecf3a5bcfb5ce4b21623342bdf1a23
Noah Gibbs <the.codefolio.guy@gmail.com>
2019-03-01 14:36:23 -0700

    Use new controllers

---
+++ app/q_controller.rb
@@ -0,0 +1,6 @@
+class QController <
+        R00lz::Controller
+  def a_quote
+   "All You Need Is Love."
+  end
+end
--- config.ru
+++ config.ru
@@ -1,2 +1,3 @@
 require_relative "config/app"
+require_relative "app/q_controller"
 run Quotes::App.new
