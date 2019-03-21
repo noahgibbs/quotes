88babe2654bf14287e03e4539bec9a9a020b79e5
Noah Gibbs <the.codefolio.guy@gmail.com>
2019-03-20 14:46:11 -1000

    Use new params method

--- app/q_controller.rb
+++ app/q_controller.rb
@@ -4,6 +4,11 @@ class QController <
    "You can't take the sky from me."
   end
 
+  def card_trick
+    n = params["card"] || "Queen"
+    "Your card: the #{n} of spades!"
+  end
+
   def shakes
     @noun = :winking
     render(:shakes)
